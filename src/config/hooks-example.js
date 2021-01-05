9098410ab14b2aafc21222213c06cefa9d00ba10
Amon Caldas <amoncaldas@gmail.com>
2021-01-05 12:52:14 +0100

    Added catch all hook

--- src/config/hooks-example.js
+++ src/config/hooks-example.js
@@ -1,6 +1,8 @@
 // This is an example file and is expected to be cloned 
 // without the -example on the same folder that it resides.
 
+/* THE CODE BELOW IS COMMENTED BECAUSE THEY ARE JUST EXAMPLES
+
 import main from '@/main'
 import store from '@/store/store'
 import appConfig from '@/config/app-config'
@@ -26,6 +28,26 @@ appHooks.add('appLoaded', (vueInstance) => {
   // Do something when the app is loaded
 }, 1)
 
+// The catch all hook allows, as the name says, cathing all
+// the hooks. It receives as parameters not only the hook parameter
+// arg, but also the hook name. It can be used to run plugin functions 
+// with the same name of the hook. By using this hook in combination with 
+// a plugin you avoid having to define every hook individually and inside
+// these hooks call a plugin method/function.
+
+appHooks.add('catchAll', (hookName, hookData) => {
+  if (pluginExample && typeof pluginExample[hookName] === 'function') {
+    return pluginExample[hookName](hookData)
+  } else {
+    return hookData // this return is always necessary
+  }
+}, 1)
+
+// #### INDIVIDUAL HOOKS DEFINITION ###
+
+// If you want to define the hooks individually, 
+// then the templates are shown below.
+
 appHooks.add('mapViewDataChanged', (mapViewData) => {
   pluginExample.mapViewDataChanged(mapViewData)
 }, 1)
@@ -44,10 +66,9 @@ appHooks.add('loadMenuItems', () => {
   })
 }, 1)
 
-/**
- * If the app is set not to use load and use ORS menu
- * then add two menu items mock p to show that menu  is working
- */
+
+// If the app is set not to use load and use ORS menu
+// then add two menu items mock p to show that menu  is working
 appHooks.add('modifyMenu', (menu) => {
   // If in app settings is set not  to load/use ORS menu items, then
   // two items will be added to the menu as a sample. It is just to 
@@ -151,22 +172,23 @@ appHooks.add('markersCreated', (markers) => {
 
   // Code example for customizing icons using material desing icons https://material.io/resources/icons/
 
-  /* # Beginning of the example
-  let markerIcoStyle = 'transform: rotate(+45deg); margin-left: 4px;margin-top: 2px;'
+  // ### BEGINNING OF EXAMPLE
 
-  for (let key in markers) {
-    let markerColor = '#c30b82' // maybe change the color based on the place properties ?
-    let iconDivMarkerStyle = `width: 30px;height: 30px;border-radius: 50% 50% 50% 0;background: ${markerColor};position: absolute;transform: rotate(-45deg);left: 50%;top: 50%;margin: -15px 0 0 -15px;`
-    let markerIcon = 'hotel' // maybe change the icon based on the place properties ?
+  // let markerIcoStyle = 'transform: rotate(+45deg); margin-left: 4px;margin-top: 2px;'
+
+  // for (let key in markers) {
+  //   let markerColor = '#c30b82' // maybe change the color based on the place properties ?
+  //  let iconDivMarkerStyle = `width: 30px;height: 30px;border-radius: 50% 50% 50% 0;background: ${markerColor};position: absolute;transform: rotate(-45deg);left: 50%;top: 50%;margin: -15px 0 0 -15px;`
+  //  let markerIcon = 'hotel' // maybe change the icon based on the place properties ?
     
-    markers[key].icon = Leaflet.divIcon({
-      className: 'custom-div-icon',
-      html: `<div style='${iconDivMarkerStyle}'><i style='${markerIcoStyle}' class='material-icons'>${markerIcon}</i></div>`,
-      iconSize: [30, 42],
-      iconAnchor: [15, 42]
-    })
-  } 
-  # end of example */
+  //  markers[key].icon = Leaflet.divIcon({
+  //    className: 'custom-div-icon',
+  //    html: `<div style='${iconDivMarkerStyle}'><i style='${markerIcoStyle}' class='material-icons'>${markerIcon}</i></div>`,
+  //    iconSize: [30, 42],
+  //    iconAnchor: [15, 42]
+  //  })
+  // ### END OF EXAMPLE
+  // }
   return markers
 }, 1)
 
@@ -208,48 +230,52 @@ appHooks.add('mapCenterChanged', (latlng) => {
 
 appHooks.add('beforeOpenMarkerPopup', (hookData) => {
   // hookData has the following structure {marker: Object, markerPopupContainer: HtmlNode}
-  // markerPopupContainer = an HTML node and can be manipulated,
+  // `markerPopupContainer` is an HTML node and can be manipulated.
   // It possible to remove and change existing content, as well
   // as add new vuejs component. See the example below.
 
-  /* ## Beginning of example
+  // ### BEGINNING OF EXAMPLE
 
-  // the line below would remove the inner text
-  markerPopupContainer.innerText = ''
+  // The line below would remove the inner text
+
+  // markerPopupContainer.innerText = ''
 
   // the line below would remove the inner html
-  markerPopupContainer.innerHTML = ''
+
+  // markerPopupContainer.innerHTML = ''
   
   // this hook will be invoked every time the popup is about
   // to render. So, to avoid content multiple times
   // we check it the component was not already added
-  let buttons = markerPopupContainer.querySelectorAll('button')
+  
+  //let buttons = markerPopupContainer.querySelectorAll('a')
 
   // If the vuejs component was not already added
-  if (buttons.length == 0) {
-    // Don't forget to the the imports in the header 
-    // for this example, it would be: 
-    // import Vue from 'vue'
-    // import {VBtn} from 'vuetify'
-    var ComponentClass = Vue.extend(VBtn) // don't forget to add 
-    var instance = new ComponentClass({ propsData: { color: 'primary' } })
-    instance.$slots.default = [ 'Click me!' ]
-    instance.$on(['click'], e => { console.log('event', e) })
-    instance.$mount() // pass nothing
+  // if (buttons.length == 0) {
+  // Don't forget to the the imports in the header 
+  // for this example, it would be: 
+  // import Vue from 'vue'
+  // import {VBtn} from 'vuetify'
+  //  var ComponentClass = Vue.extend(VBtn) // don't forget to add 
+  //  var instance = new ComponentClass({ propsData: { color: 'primary' } })
+  //  instance.$slots.default = [ 'Click me!' ]
+  //  instance.$on(['click'], e => { console.log('event', e) })
+  //  instance.$mount() // pass nothing
    
     // Finnaly append the instantiated vue 
     // component to the markerPopupContainer
-    markerPopupContainer.appendChild(instance.$el)
-  } 
-  ## end of example */ 
+    // markerPopupContainer.appendChild(instance.$el)
+  // } 
+  // ### END OF EXAMPLE
 }, 1)
 
 appHooks.add('beforeShowResolvedPlaceInfo', (hookData) => {
-  /*
-  hookData has the following structure: { 
-    placeInfo: { placeName: String, containerArea: String, clickInsidePolygon: Boolean, latlng: {lat:..., lng:...}},
-    placeInfoContainer: HtmlNode
-  } */
+  
+  // hookData has the following structure: 
+  // { 
+  //  placeInfo: { placeName: String, containerArea: String, clickInsidePolygon: Boolean, latlng: {lat:..., lng:...}},
+  //  placeInfoContainer: HtmlNode
+  // }
   // If the placeInfo.customHtml prop is set,
   // its content will be shown in the place
   // info box. If the prop hideDirectionsTo is set
@@ -312,25 +338,18 @@ appHooks.add('placeSearchPromisesDefined', (promises) => {
   // Two promises, one for address/admistrative localities and other for venus are created.
   // You could pottentially replace one of them or both for custom requests.
 
-  /* Example:
-  let filters = { category_group_ids: [100] } // define a category that would be searched
-  let promise = Pois(filters, 100) // you would have to import pois, like: import { Pois } from '@/support/ors-api-runner'
-  return [promise]
-  */
+  // ### BEGINNING OF EXAMPLE
+  // let filters = { category_group_ids: [100] } // define a category that would be searched
+  // let promise = Pois(filters, 100) // you would have to import pois, like: import { Pois } from '@/support/ors-api-runner'
+  // return [promise]
+  // ### END OF EXAMPLE
  return promises
 })
 
+appHooks.add('beforeUseMarkerClusterOptions', (options) => {
+  // Do something
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // An options object must bereturned
+  return options
+})
+*/
