55fb203af4f3d8dfdc395563c935e2542a100faa
Amon Caldas <amoncaldas@gmail.com>
2021-01-12 17:36:34 +0100

    Remove unecessary plugin

--- src/plugins/target-pois/fragments/html-icon/HtmlIcon.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div :style="{background: color}" class="target-poi-html-icon-div">
-      <img class="target-poi-html-icon-img" :src="iconImg">
-    </div>
-  </div>
-</template>
-<script src="./html-icon.js"></script>
-<style src="./html-icon.css"></style>
