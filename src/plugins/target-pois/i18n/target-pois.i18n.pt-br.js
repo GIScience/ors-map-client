7772c5c8ab1a5fb4267b89dce565362d1abc4a14
Amon Caldas <amoncaldas@gmail.com>
2021-01-14 11:00:21 +0100

    Updated base client

---
+++ src/plugins/target-pois/i18n/target-pois.i18n.pt-br.js
@@ -0,0 +1,25 @@
+export default {
+  targetPois: {
+    'name':'Nome',
+    'amenity': 'Instalação',
+    'addr:city': 'Cidade',
+    'addr:country': 'País',
+    'addr:housenumber': 'Número de porta',
+    'addr:postcode': 'Código postal',
+    'addr:street': 'Rua',
+    'healthcare': 'Unidade de saúde',
+    'healthcare:speciality': 'Tipo de unidade de saúde',    
+    'opening_hours': 'Horário de abertura',
+    'vaccination': 'Vacinação',
+    routeToThisPlace: 'Direções para este local',
+    loadingPois: 'Carregando centros de vacinação para a região visível ...',
+    poisLoaded: 'Centros de vacinação carregados',
+    notPoisInThisRegion: 'Não foram encontrados centros de vacinaçào nesta região. Mova o mapa ou busque por um local.',
+    searchingClosestPoi: 'Buscando o centro de vacinaão mais próximo',
+    notPoiFoundNearTheLocationSelected: 'Nenhum centro de vacinaçào encontrado próximo ao local selecionado',
+    findPoiNear: 'Vacinação próxim a',
+    zoomInToLoadPOIs: 'Dê zoom em uma região para carregar os centros de vacinação',
+    poisForTheCurrentViewCouldNotBeLoaded: 'Não foi possível carregar locais de vacinação para a região visível no map'
+  }
+}
+
