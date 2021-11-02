14f6ffdeaeb10da4f31218f61dd1aa0f7572a49d
Amon Caldas <amoncaldas@gmail.com>
2021-11-02 15:20:21 +0100

    refactor: move the maps modal css classes to box element

--- src/pages/maps/Maps.vue
+++ src/pages/maps/Maps.vue
@@ -52,15 +52,15 @@
       </places-carousel>
     </v-bottom-nav>
 
-    <v-dialog v-model="isSettingsOpen" max-width="600" class="settings-modal" :persistent="true" attach="body">
-      <box background="white" v-if="isSettingsOpen" resizable closable @closed="closeSettingsModal()">
+    <v-dialog v-model="isSettingsOpen" max-width="600" :persistent="true" attach="body">
+      <box background="white" v-if="isSettingsOpen" class="settings-modal" resizable closable @closed="closeSettingsModal()">
         <h3 slot="header">{{$t('maps.settings')}}</h3>
         <settings @saved="closeSettingsModal"></settings>
       </box>
     </v-dialog>
 
-    <v-dialog v-model="isAboutOpen" max-width="600" class="about-modal" :persistent="true" attach="body">
-      <box background="white" v-if="isAboutOpen" resizable closable @closed="closeAboutModal()">
+    <v-dialog v-model="isAboutOpen" max-width="600" :persistent="true" attach="body">
+      <box background="white" v-if="isAboutOpen" class="about-modal" resizable closable @closed="closeAboutModal()">
         <h3 slot="header">{{$t('maps.aboutTitle')}}</h3>
         <about></about>
       </box>
