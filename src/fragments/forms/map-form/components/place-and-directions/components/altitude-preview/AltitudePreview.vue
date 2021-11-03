df417dee39975af91481178a9327eced8a708803
Amon Caldas <amoncaldas@gmail.com>
2021-11-03 17:53:16 +0100

    feat(altitudepreview.vue): add a class to the expand button

--- src/fragments/forms/map-form/components/place-and-directions/components/altitude-preview/AltitudePreview.vue
+++ src/fragments/forms/map-form/components/place-and-directions/components/altitude-preview/AltitudePreview.vue
@@ -1,7 +1,7 @@
 <template>
   <box background="white" no-shadow>
     <div slot="header">
-      <v-btn style="float:right; margin-top:0" small icon :title="$t('altitudePreview.expandAltitudeChart')" @click="openAltitudeBox()"><v-icon>open_in_new</v-icon></v-btn>
+      <v-btn style="float:right; margin-top:0" class="expand-altitude-btn" small icon :title="$t('altitudePreview.expandAltitudeChart')" @click="openAltitudeBox()"><v-icon>open_in_new</v-icon></v-btn>
       <h3>{{$t('altitudePreview.altitude')}}</h3>
     </div>
     <altitude :height="height" :map-view-data="mapViewData"></altitude>
