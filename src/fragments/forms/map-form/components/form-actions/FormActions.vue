c3af03af9a40c3060b6ab77eb0cf27e80ed07e7c
Amon Caldas <amoncaldas@gmail.com>
2021-11-04 16:27:35 +0100

    refactor(formactions.vue): add class to each map-form-btn

--- src/fragments/forms/map-form/components/form-actions/FormActions.vue
+++ src/fragments/forms/map-form/components/form-actions/FormActions.vue
@@ -2,17 +2,17 @@
   <div class="form-actions">
     <v-layout class="action-options-wrapper">
       <v-flex :class="{'high-resolution': $highResolution}" v-if="canAddPlaceInput && isEnabled('addPlaceInput')">
-        <map-form-btn @clicked="addPlaceInput" :icon="'add_location'" :title="$t('formActions.addPlaceInput')">
+        <map-form-btn @clicked="addPlaceInput" :icon="'add_location'" class="add-place-btn" :title="$t('formActions.addPlaceInput')">
         </map-form-btn>
       </v-flex>
       <v-flex :class="{'high-resolution': $highResolution}" v-if="$store.getters.mode === constants.modes.directions && isEnabled('clearPlaces')">
-        <map-form-btn @clicked="clearPlaces" :icon="'clear_all'" :title="$t('formActions.clearRoute')"></map-form-btn>
+        <map-form-btn @clicked="clearPlaces" :icon="'clear_all'" class="clear-route-btn" :title="$t('formActions.clearRoute')"></map-form-btn>
       </v-flex>
       <v-flex :class="{'high-resolution': $highResolution}" v-if="$store.getters.mode === constants.modes.directions && isEnabled('reverseRoute')">
-        <map-form-btn @clicked="reverseRoute" :icon="'repeat'" :title="$t('formActions.reverseRoute')"></map-form-btn>
+        <map-form-btn @clicked="reverseRoute" :icon="'repeat'" class="reverse-route-btn" :title="$t('formActions.reverseRoute')"></map-form-btn>
       </v-flex>
       <v-flex :class="{'high-resolution': $highResolution}" v-if="isEnabled('roundtrip')">
-        <map-form-btn :text="$t('formActions.roundTrip')" :title="$t('formActions.toggleRoundTrip')"
+        <map-form-btn :text="$t('formActions.roundTrip')" class="round-trip-btn" :title="$t('formActions.toggleRoundTrip')"
           @clicked="toggleRoundTrip" :icon="'settings_backup_restore'"
           :color="roundtripActive ? 'primary' : 'default'"></map-form-btn>
       </v-flex>
