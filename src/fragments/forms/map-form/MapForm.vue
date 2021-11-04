0c9116b14d0bfcad55835d4bdf4a62539befde9e
Amon Caldas <amoncaldas@gmail.com>
2021-11-04 16:26:43 +0100

    refactor(mapform.vue): add class to tab content containers

--- src/fragments/forms/map-form/MapForm.vue
+++ src/fragments/forms/map-form/MapForm.vue
@@ -9,10 +9,10 @@
       </v-tab>
     </v-tabs>
     <v-tabs-items v-model="activeTab" touchless>
-      <v-tab-item key="0" v-if="hasPlacesAndDirectionsTab" touchless>
+      <v-tab-item key="0" class="places-and-directions-tab-form" v-if="hasPlacesAndDirectionsTab" touchless>
         <places-and-directions :active="$store.getters.mode !== constants.modes.isochrones"> </places-and-directions>
       </v-tab-item>
-      <v-tab-item key="1" v-if="hasIsochronesTab" touchless>
+      <v-tab-item key="1" class="isochrones-tab-form" v-if="hasIsochronesTab" touchless>
         <isochrones :active="$store.getters.mode === constants.modes.isochrones" ></isochrones>
       </v-tab-item>
     </v-tabs-items>
