fc8a2ea13b4ac94b7af57c5621b404246f7e3af0
Amon Caldas <amoncaldas@gmail.com>
2021-11-05 15:38:43 +0100

    test: adjust karma debug config for integration and unit tests

--- tests/unit/karma.debug.conf.js
+++ tests/unit/karma.debug.conf.js
@@ -7,6 +7,13 @@ baseKarmaDebugConf.files = [
 
 baseKarmaDebugConf.coverageReporter = null
 
+// Do not include `coverage` reporter, since the coverage
+// is only generated when running the all-tests command
+baseKarmaDebugConf.preprocessors = {
+  'specs/*[sS]pec.js': ['webpack'],
+}
+
+
 // eslint-disable-next-line no-undef
 module.exports = function (config) {
   baseKarmaDebugConf.logLevel = config.LOG_DEBUG, //config.LOG_DISABLE, config.LOG_ERROR, config.LOG_INFO, config.LOG_DEBUG
