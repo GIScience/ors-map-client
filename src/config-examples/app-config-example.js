693162e2f316381cdcee016a23f58c729be4cc31
Amon Caldas <amoncaldas@gmail.com>
2021-11-25 12:08:09 +0100

    fix: update cyclosm tile url in config example

--- src/config-examples/app-config-example.js
+++ src/config-examples/app-config-example.js
@@ -98,7 +98,7 @@ const appConfig = {
       name: 'Cyclosm',
       id: 'cyclosm',
       visible: false,
-      url: 'https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+      url: 'https://{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
       attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }
   ],
