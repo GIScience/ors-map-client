efb008134701c8bb1fd7e999cc611086c4520418
Amon Caldas <amoncaldas@gmail.com>
2021-12-01 12:05:40 +0100

    refactor(routeimporter.vue): add css class to elements

--- src/fragments/forms/route-importer/RouteImporter.vue
+++ src/fragments/forms/route-importer/RouteImporter.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <map-form-btn @clicked="openImporter()" :icon="'cloud_upload'" :title="$t('routeImporter.loadRouteFile')" ></map-form-btn>
+  <div class="route-importer-container">
+    <map-form-btn class="route-importer-btn" @clicked="openImporter()" :icon="'cloud_upload'" :title="$t('routeImporter.loadRouteFile')" ></map-form-btn>
     <v-dialog v-model="isImportModalOpen" max-width="600" :persistent="true" attach="body">
-      <box v-model="isImportModalOpen" background="white" closable @closed="closeImporter()">
+      <box customClass="route-importer-modal" v-model="isImportModalOpen" background="white" closable @closed="closeImporter()">
         <h3 slot="header">{{$t('routeImporter.loadRouteFile')}}</h3>
         <vue-dropzone ref="importRouteDropzone" @vdropzone-file-added="fileAdded" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
         <v-alert :value="true" type="info" style="color:white" >
