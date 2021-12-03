8fbd0b3ffeffbaafd91554c53ff4eafe4fd00eb5
Amon Caldas <amoncaldas@gmail.com>
2021-12-03 17:08:31 +0100

    test(share.spec.js): extend share component test

--- tests/integration/specs/share.spec.js
+++ tests/integration/specs/share.spec.js
@@ -10,6 +10,49 @@ import store from '@/store/store'
 import '@babel/polyfill'
 
 describe('Share', () => {
+  it('should open and close share modal', async (done) => {
+    let directionsUrl = `${constants.orsPublicHost}/#/directions/Mannheim,BW,Germany/Heidelberg,BW,Germany/data/%7B"coordinates":"8.492765,49.488789;8.692416,49.401247","options":%7B"zoom":8,"profile":"driving-car","preference":"recommended"%7D%7D`
+    var shareProps = {
+      url: directionsUrl
+    }
+
+    await new AppLoader().fetchApiInitialData()
+    const wrapper = mount(Share, {i18n: I18nBuilder.build(), store: store, propsData: shareProps })
+
+    expect(wrapper.contains('.share-container')).toBe(true)
+    expect(wrapper.findComponent(Share).exists()).toBe(true)    
+    await wrapper.find('.open-share-btn').trigger('click')
+    let shareModal = document.querySelector('.share-modal')
+    expect(shareModal).toBeTruthy()     
+    await wrapper.find('.share-modal .box-header button').trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(document.querySelector('.share-modal')).not.toBeTruthy()
+    done()  
+  })
+
+  it('should open and copy share url', async (done) => {
+    let directionsUrl = `${constants.orsPublicHost}/#/directions/Mannheim,BW,Germany/Heidelberg,BW,Germany/data/%7B"coordinates":"8.492765,49.488789;8.692416,49.401247","options":%7B"zoom":8,"profile":"driving-car","preference":"recommended"%7D%7D`
+    var shareProps = {
+      url: directionsUrl
+    }
+
+    await new AppLoader().fetchApiInitialData()
+    const wrapper = mount(Share, {i18n: I18nBuilder.build(), store: store, propsData: shareProps })
+
+    expect(wrapper.contains('.share-container')).toBe(true)
+    expect(wrapper.findComponent(Share).exists()).toBe(true)    
+    await wrapper.find('.open-share-btn').trigger('click')
+    expect(document.querySelector('.share-modal')).toBeTruthy()
+    
+    var shareModal = document.querySelector('.share-modal')
+    expect(shareModal).toBeTruthy()
+    shareModal.querySelector('input[type="text"]').click()
+    await new Promise(resolve => setTimeout(resolve, 200))
+    // let clipboard = await navigator.clipboard.readText()
+    // expect(clipboard).toBeTruthy()
+    done()  
+  })
+
   it('should render share component and toggle short url', async (done) => {
     let directionsUrl = `${constants.orsPublicHost}/#/directions/Mannheim,BW,Germany/Heidelberg,BW,Germany/data/%7B"coordinates":"8.492765,49.488789;8.692416,49.401247","options":%7B"zoom":8,"profile":"driving-car","preference":"recommended"%7D%7D`
     var shareProps = {
@@ -34,10 +77,16 @@ describe('Share', () => {
     expect(shareUrlTextarea.value).toContain(iframeCode)    
     await wrapper.find('.toggle-short-url').trigger('click')
 
-    await shareModal.__vue__.$nextTick()
+    await wrapper.vm.$nextTick()
     await new Promise(resolve => setTimeout(resolve, 1000))
     shareUrlInput = shareModal.querySelector('input[type="text"]')
     expect(shareUrlInput.value).toContain('https://bit.ly/')
+
+    await wrapper.find('.toggle-short-url').trigger('click')
+    await wrapper.vm.$nextTick()
+    // await new Promise(resolve => setTimeout(resolve, 1000))
+    shareUrlInput = shareModal.querySelector('input[type="text"]')
+    expect(shareUrlInput.value).not.toContain('https://bit.ly/')
     done()  
   })
 })
