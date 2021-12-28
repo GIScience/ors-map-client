f24e8f0407b04f79fed56340e085636fd1476821
Amon Caldas <amoncaldas@gmail.com>
2021-12-28 13:31:55 +0100

    refactor(download.vue): add add class to elements

--- src/fragments/forms/map-form/components/download/Download.vue
+++ src/fragments/forms/map-form/components/download/Download.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <v-btn style="float:right; margin-top:0" small icon :title="$t('download.download')" @click="openDownload()"><v-icon>cloud_download</v-icon></v-btn>
+  <div class="download-container">
+    <v-btn class="open-download-btn" style="float:right; margin-top:0" small icon :title="$t('download.download')" @click="openDownload()"><v-icon>cloud_download</v-icon></v-btn>
     <v-dialog v-model="isDownloadModalOpen" max-width="600" attach="body" :persistent="true">
-      <box v-model="isDownloadModalOpen" background="white" closable @closed="closeDownload()">
+      <box customClass="download-modal" v-model="isDownloadModalOpen" background="white" closable @closed="closeDownload()">
         <h3 slot="header">{{$t('download.downloadRoute')}}</h3>
-        <v-text-field :label="$t('download.downloadFileName')" v-model="downloadFileName" :required="true"></v-text-field>
-        <v-select :label="$t('download.downloadFormat')" :items="availableDownloadFormats" v-model="downloadFormat"></v-select>
+        <v-text-field class="export-file-name" :label="$t('download.downloadFileName')" v-model="downloadFileName" :required="true"></v-text-field>
+        <v-select class="download-format" :label="$t('download.downloadFormat')" :items="availableDownloadFormats" v-model="downloadFormat"></v-select>
         <v-layout row wrap>
           <v-spacer></v-spacer>
           <v-flex xs12 sm6>
-            <v-btn color="primary" style="float:right; margin-top:0" :title="$t('download.download')" @click="download()">{{$t('download.download')}}</v-btn>
+            <v-btn class="download" color="primary" style="float:right; margin-top:0" :title="$t('download.download')" @click="download()">{{$t('download.download')}}</v-btn>
           </v-flex>
         </v-layout>
       </box>
