f8c2cce8164c04bd0d38aebfe2c315f570776775
Amon Caldas <amoncaldas@gmail.com>
2022-01-18 11:14:53 +0100

    test(place-input.spec.js): extend place-input test coverage

--- tests/integration/specs/place-input.spec.js
+++ tests/integration/specs/place-input.spec.js
@@ -31,14 +31,7 @@ describe('Place-input', () => {
     height: 30,
     mb: 0,
     disabled: false
-  }  
-
-  it('should render place-input component with `find a place` label', async (done) => {
-    await new AppLoader().fetchApiInitialData()
-    const wrapper = await render(PlaceInput, {propsData: placeInputProps, i18n: i18n, store: store })
-    expect(wrapper.text()).toContain(i18n.messages[i18n.locale].placeInput.findAPlace)
-    done()
-  })
+  } 
 
   it('should render place-input component with `startingPlace`', async (done) => {
     await new AppLoader().fetchApiInitialData()
@@ -60,14 +53,79 @@ describe('Place-input', () => {
     await textInput.setValue('heidelberg')
     await textInput.trigger('keyup')
 
-    // When the input has completed the autocomplete task
-    wrapper.vm.$on('autocompleted', async () => {
-      // Wait a bit so that suggestions are rendered
-      await new Promise(resolve => setTimeout(resolve, 200))
-      expect(wrapper.findAll('.place-suggestion').length).toBeGreaterThan(2)
-      expect(wrapper.vm.localModel.suggestions.length).toBeGreaterThan(2)
-      expect(wrapper.vm.placeSuggestions.length).toBeGreaterThan(2)
-      done()
-    })
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    expect(wrapper.findAll('.place-suggestion').length).toBeGreaterThan(2)
+    expect(wrapper.vm.localModel.suggestions.length).toBeGreaterThan(2)
+    expect(wrapper.vm.placeSuggestions.length).toBeGreaterThan(2)
+    done()
   })
+
+  it('should show suggestions when coordinates are inputted', async (done) => {
+    placeInputProps.isLast = false
+    await new AppLoader().fetchApiInitialData()
+    const wrapper = mount(PlaceInput, {propsData: placeInputProps, i18n: i18n, store: store })   
+    
+    expect(wrapper.contains('.place-input')).toBe(true)
+    expect(wrapper.findComponent(PlaceInput).exists()).toBe(true)
+
+    const textInput = wrapper.find('input[type="text"]')    
+    await textInput.setValue('-37.97321319580079,-12.489208068615273')
+    await textInput.trigger('keyup')
+
+    await new Promise(resolve => setTimeout(resolve, 4000))
+    expect(wrapper.findAll('.place-suggestion').length).toBeGreaterThan(2)
+    let rawCoordinates = wrapper.findAll('.place-suggestion').wrappers[0].find('.raw-coord .v-list__tile__title button strong')
+    expect(rawCoordinates.element.innerText).toBe('-37.97321319580079,-12.489208068615273')
+    expect(wrapper.vm.localModel.suggestions.length).toBeGreaterThan(2)
+    expect(wrapper.vm.placeSuggestions.length).toBeGreaterThan(2)
+
+    await wrapper.findAll('.switch-coords').trigger('click')
+    await new Promise(resolve => setTimeout(resolve, 3000))
+    rawCoordinates = wrapper.findAll('.place-suggestion').wrappers[0].find('.raw-coord .v-list__tile__title button strong')
+    expect(rawCoordinates.element.innerText).toBe('-12.489208068615273,-37.97321319580079')  
+    expect(wrapper.emitted().autocompleted).toBeTruthy() 
+    
+    let switchCoords =  wrapper.find('.switch-coords')
+    await switchCoords.trigger('click')
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    let suggestions = wrapper.findAll('.place-suggestion')
+    expect(suggestions.length).toBe(2)      
+    done()
+  })  
+
+  it('should not show suggestions when no text is inputted', async (done) => {
+    placeInputProps.isLast = false
+    await new AppLoader().fetchApiInitialData()
+    const wrapper = mount(PlaceInput, {propsData: placeInputProps, i18n: i18n, store: store })   
+    
+    expect(wrapper.contains('.place-input')).toBe(true)
+    expect(wrapper.findComponent(PlaceInput).exists()).toBe(true)
+
+    const textInput = wrapper.find('input[type="text"]')    
+    await textInput.setValue('')
+    await wrapper.trigger('keydown.enter')
+
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    expect(wrapper.findAll('.place-suggestion').length).toBe(0)    
+    done()
+  }) 
+
+  it('should switch to search mode on search action', async (done) => {
+    placeInputProps.isLast = false
+    placeInputProps.supportSearch = true
+    await new AppLoader().fetchApiInitialData()
+    const wrapper = mount(PlaceInput, {propsData: placeInputProps, i18n: i18n, store: store })   
+    
+    expect(wrapper.contains('.place-input')).toBe(true)
+    expect(wrapper.findComponent(PlaceInput).exists()).toBe(true)
+
+    const textInput = wrapper.find('input[type="text"]')    
+    await textInput.setValue('heidelberg') 
+    await wrapper.findAll('.place-input .search').trigger('click')
+
+    wrapper.vm.$nextTick()
+    expect(wrapper.findAll('.place-suggestion').length).toBe(0)       
+    expect(wrapper.emitted().switchedToSearchMode).toBeTruthy() 
+    done()
+  }) 
 })
