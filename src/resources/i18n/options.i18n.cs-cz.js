1734417f2079e5596426fcd6e1a636f16b74b04a
trendspotter <trendspotter@users.noreply.github.com>
2022-03-24 17:51:35 +0100

    Add Czech language

---
+++ src/resources/i18n/options.i18n.cs-cz.js
@@ -0,0 +1,14 @@
+export default {
+  options: {
+    units: {
+      kilometers: 'kilometry',
+      meters: 'metry',
+      miles: 'míle'
+    },
+    areUnits: {
+      sqKms: 'km²',
+      hectare: 'hektar',
+      sqMeters: 'm²'
+    }
+  }
+}
