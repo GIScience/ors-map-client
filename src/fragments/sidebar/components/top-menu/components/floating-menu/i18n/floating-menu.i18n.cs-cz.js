63b7e3ec2defd0e34ea378e0d28e2f7528b6a33d
trendspotter <trendspotter@users.noreply.github.com>
2022-03-24 20:36:37 +0100

    Add Czech language

---
+++ src/fragments/sidebar/components/top-menu/components/floating-menu/i18n/floating-menu.i18n.cs-cz.js
@@ -0,0 +1,11 @@
+export default {
+  floatingTopMenu: {
+    openSettings: 'Nastavení',
+    showAbout: 'Zobrazit obecné informace',
+    about: 'O ORS',
+    openrouteserviceAPI: 'Openrouteservice API',
+    openrouteserviceForDisasters: 'Openrouteservice pro katastrofy',
+    askOpenrouteservice: 'Ptejte se openrouteservice',
+    settingsTooltip: 'Pokud chcete upravit nastavení aplikace a zobrazit další možnosti, klikněte zde.'
+  }
+}
