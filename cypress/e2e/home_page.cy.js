ec56b4e86728be39270dfdd1cf26c1d26cce3ddb
Amandus Butzer <amandus.butzer@heigit.org>
2022-12-02 14:38:00 +0100

    test: Move sidebar toggle test
    testing the side bar toggle was still done in the directions test
    this should be done in a more general layout test.
    re-toggling currently doesn't work due to a detached element and
    can be investigated later

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -4,9 +4,19 @@ import appConfig from '../../src/config/app-config'
 
 describe('Ors map client', () => {
   it('successfully loads', () => {
+    cy.viewport(1848, 980)
     cy.visit('/')
     let coords = defaultMapSettings.mapCenter
     cy.url().should('contain', coords.lat).should('contain', coords.lng).
       should('contain', appConfig.initialZoomLevel.toString())
+  })
+  it('toggles the sidebar correctly', () => {
+    cy.get('.sidebar').should('not.be.visible')
+    cy.get('.toggle-sidebar').as('sb')
+    cy.get('@sb').click()
+    cy.get('.sidebar').should('be.visible')
+    // TODO: get re-toggle test working
+    // cy.get('@sb').click()
+    // cy.get('.sidebar').should('not.be.visible')
   })
 })
