4c77f8a4b2bee5ef4d046f7b094d960cb7d193ee
Christian Eichert <102165928+ecxod@users.noreply.github.com>
2023-02-02 09:42:45 +0100

    Update maps.i18n.es-es.js
    Removed some ticks

--- src/pages/maps/i18n/maps.i18n.es-es.js
+++ src/pages/maps/i18n/maps.i18n.es-es.js
@@ -1,16 +1,16 @@
 
 export default {
   maps: {
-    'pageTitle': 'ORS Maps',
-    'Places': 'Lugares',
-    'isochrones': 'Isócronas',
-    'placeNameRequired': 'Nombre del lugar solicitado',
-    'altitude': 'Variación de la altitud',
-    'settings': 'Opciones',
-    'toggleAccessibleMode': 'Activar/desactivar el modo de accesibilidad',
-    'about': 'Acerca de ORS',
-    'updateSearch': 'Buscar en esta zona',
-    'updateSearchAfterZoomOrCenterChange': 'Refrescar la búsqueda después de cambiar el centro',
-    'pageNotFound': 'Página no encontrada. Verifique que la URL no esté incompleta o sea incorrecta'
+    pageTitle: 'ORS Maps',
+    Places: 'Lugares',
+    isochrones: 'Isócronas',
+    placeNameRequired: 'Nombre del lugar solicitado',
+    altitude: 'Variación de la altitud',
+    settings: 'Opciones',
+    toggleAccessibleMode: 'Activar/desactivar el modo de accesibilidad',
+    about: 'Acerca de ORS',
+    updateSearch: 'Buscar en esta zona',
+    updateSearchAfterZoomOrCenterChange: 'Refrescar la búsqueda después de cambiar el centro',
+    pageNotFound: 'Página no encontrada. Verifique que la URL no esté incompleta o sea incorrecta'
   }
 }
