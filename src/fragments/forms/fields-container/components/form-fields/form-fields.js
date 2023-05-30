GIScience/ors-map-client

commit 222423eca3ec4904256699ad50f66b9dedf5aeca

Added french Removed paved/unpaved avoidables

src/fragments/forms/fields-container/components/form-fields/form-fields.js
@@ -271,9 +271,7 @@ export default {
               }
             }
             // If the item object has texts for the current locale, get it and then set it as the itemtext
-            var itemText = item[this.$store.getters.mapSettings.locale] || item[defaultMapSettings.locale]  
-            item.itemValue = item[parameter.itemValue]
-            item.itemText = itemText || item.itemValue
+            item.itemText = item[this.$store.getters.mapSettings.locale] || item[defaultMapSettings.locale] || item.itemValue
           } else { // item is not an object, but a simple value
             let itemObj = {
               itemValue: item,

src/config-examples/ors-map-filters-example.js
@@ -504,8 +504,6 @@ const filters = [
           'tollways',
           'ferries',
           'tunnels',
-          'pavedroads',
-          'unpavedroads',
           'tracks',
           'fords',
           'steps',
@@ -519,9 +517,7 @@ const filters = [
                 'highways',
                 'tollways',
                 'ferries',
-                'tunnels',
-                'pavedroads',
-                'unpavedroads'
+                'tunnels'
               ],
               'cycling-*': [
                 'ferries',

src/config-examples/settings-options-example.js
@@ -34,7 +34,8 @@ const settingsOptions = {
     { text: 'English US', value: 'en-us' },
     { text: 'Deutsch DE', value: 'de-de' },
     { text: 'Português BR', value: 'pt-br' },
-    { text: 'Italiano IT', value: 'it-it' }
+    { text: 'Italiano IT', value: 'it-it' },
+    { text: 'Français FR', value: 'fr-fr' }
   ],
   tileServices: [
     { text: 'OpenStreetMap', value: 'osm' },

src/fragments/about/i18n/about.i18n.fr-fr.js
@@ -1,0 +1,25 @@
+
+export default {
+  "about": {
+    "donate": "Faire un don!",
+    "donateText": "Veuillez faire don de vos données géographiques à",
+    "info_I": "Openrouteservice est développé et fourni par l'Institut Heidelberg pour la technologie de géoinformation (HeiGIT) et offre des services de routage en utilisant des données géographiques gratuites générées par les utilisateurs et collectées en collaboration à partir d'OpenStreetMap. Merci de faire don de vos données géographiques à openstreetmap.org!",
+    "info_II": "Nous utilisons le stockage local du navigateur pour conserver les informations afin que vous n'ayez pas à les saisir à nouveau lors de votre visite ou lors de visites ultérieures. Nous ne collectons ni ne stockons aucune information personnelle sur vous ou des informations personnellement identifiables à votre sujet, à l'exception de ce que vous fournissez volontairement par contact direct. De plus, nous ne partageons pas de données analytiques avec des fournisseurs de services ou des partenaires.",
+    "sources": "Nous utilisons différentes bibliothèques open source pour cette application Web. Ceux-ci peuvent être trouvés dans les fichiers de package correspondants sur",
+    "feelFreeToReportBugs": "N'hésitez pas à signaler les bugs sur",
+    "orToFixThem": "ou corrigez-les vous-même et faites une pull request",
+    "info_III": "Les utilisateurs sont interdits de toute revente ou redistribution commerciale, non gratuite, sans l'autorisation écrite explicite de CIAT. Données originales de Jarvis A., H.I. Reuter, A. Nelson, E. Guevara, 2008, Données SRTM sans soudure remplies de trous V4, Centre international d'agriculture tropicale (CIAT)",
+    "info_IV": "Si vous avez d'autres questions, préoccupations ou commentaires, veuillez nous contacter via:",
+    "info_V": "Openrouteservice se soucie de vos demandes de renseignements, problèmes, questions, etc. Comme il y aura toujours un flux de demandes de renseignements, nous prenons la responsabilité de vous répondre dans les plus brefs délais. Après avoir reçu notre e-mail de réponse automatique pour vous informer que nous avons reçu votre demande, nous vous répondrons généralement sous peu. Veuillez nous donner le temps d'examiner correctement votre problème signalé avant de revenir.",
+    "CIAT_CSI_SRTM": "CIAT-CSI SRTM",
+    "dataProvided": "Données démographiques fournies par",
+    "ghsFramework": "Les cadres du Global Human Settlement (GHS)",
+    "askOpenrouteservice": "Ask Openrouteservice",
+    "termsOfService": "Conditions d'utilisation",
+    "privacyPolicy": "Politique de confidentialité",
+    "openStreetMapOrg": "OpenStreetMap.org",
+    "infoSupport": "Veuillez soutenir openrouteservice.org",
+    "supportText": "Openrouteservice propose des services gratuits et s'appuie sur des dons pour la majorité de son financement. Si vous souhaitez soutenir le développement ultérieur des fonctionnalités et les dons d'infrastructure de serveur sont les bienvenus.",
+    "softwareAndLibraries": "Logiciels et bibliothèques"
+  },
+}

src/fragments/box/i18n/box.i18n.fr-fr.js
@@ -1,0 +1,6 @@
+export default {
+  "box": {
+    "maximize": "Maximiser",
+    "minimize": "Minimiser"
+  },
+}

src/fragments/charts/altitude/i18n/altitude.i18n.fr-fr.js
@@ -1,0 +1,7 @@
+
+export default {
+  "altitude": {
+    "title": "Élévation",
+    "meters": "m"
+  },
+}

src/fragments/forms/fields-container/components/form-fields/i18n/form-fields.i18n.fr-fr.js
@@ -1,0 +1,9 @@
+export default {
+  "formFields": {
+    "fieldHelp": "Cliquez pour ouvrir la description",
+    "defaultAbbreviation": "df.: ",
+    "paramJSONContent": "Contenu JSON",
+    "wrongInputValue": "Mauvaise valeur d'entrée pour ce paramètre.",
+    "notValidJsonString": "JSON non-valide pour: fieldName"
+  },
+}

src/fragments/forms/fields-container/components/slider-combo/slider-combo.js
@@ -43,6 +43,13 @@ export default {
             context.filter.value = context.localModel = context.textLocalModel = value
             context.fieldUpdated()
           }
+        } else {
+          let defaultvalue = null
+          if (context.filter.default !== undefined) {
+            defaultvalue = context.filter.default
+          }
+          context.localModel = context.filter.value = defaultvalue
+          context.fieldUpdated()
         }
       }, 1000)
     },
@@ -66,7 +73,7 @@ export default {
         if (unitsTrans[this.filter.unit]) {
           unit = unitsTrans[this.filter.unit]
         }
-        unit = String(this.filter.unit)
+        unit = String(unit)
       }
       return unit
     },   

commit 0b97616a874b02556ee5bb1a0f91a60b80490ffd

fix: avoid_countries not working
this was due to the itemValue not being set from the parameter definition.
Accidentally broken in 222423eca3ec4904256699ad50f66b9dedf5aeca

Fixed by reintroducing the missing line

src/fragments/forms/fields-container/components/form-fields/form-fields.js
@@ -171,6 +171,7 @@ export default {
             }
             // If the item object has texts for the current locale, get it and then set it as the item text
             item.itemText = item[this.$store.getters.mapSettings.locale] || item[defaultMapSettings.locale] || item.itemValue
+            item.itemValue = item[parameter.itemValue]
           } else { // item is not an object, but a simple value
             let itemObj = {
               itemValue: item,