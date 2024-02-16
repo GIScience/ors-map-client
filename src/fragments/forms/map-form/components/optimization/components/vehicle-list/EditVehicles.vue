a1096891fc5dc6aa990b9d44c441cdf79d3993d8
Emma Hegarty <emma.hegarty@heigit.org>
2024-02-16 11:28:19 +0100

    feat: add Vehicle in editVehicles dialog
    get start via place autocomplete

--- src/fragments/forms/map-form/components/optimization/components/vehicle-list/EditVehicles.vue
+++ src/fragments/forms/map-form/components/optimization/components/vehicle-list/EditVehicles.vue
@@ -13,6 +13,9 @@
           <v-btn class="edit-vehicles-btn" flat :style="{background: 'white', 'padding-right':'15px'}" @click="saveVehicles()" :title="$t('optimization.saveVehicles')">
             <v-icon color="success">save</v-icon>
           </v-btn>
+          <v-btn class="edit-vehicles-btn" flat :style="{}" @click="addVehicle()" :title="$t('optimization.addVehicle')">
+            <v-icon color="info">add</v-icon>
+          </v-btn>
         </h3>
         <v-card @click="editId = i+1" elevation="3" style="margin: 5px;cursor: pointer" v-for="(v, i) in editVehicles" :key="i">
           <v-card-title style="padding-bottom: 0;">
@@ -25,11 +28,44 @@
             </v-btn>
           </v-card-title>
           <v-card-text>
-            <div v-if="editId !== v.id">Start: {{ v.start[0].toPrecision(8) }}, {{ v.start[1].toPrecision(8) }} -
+            <div v-if="v.start && editId !== v.id">Start: {{ v.start[0].toPrecision(8) }}, {{ v.start[1].toPrecision(8) }} -
               End: {{ v.end[0].toPrecision(8) }}, {{ v.end[1].toPrecision(8) }}</div>
             <div v-else>
-              <div v-if="v.start[0] === v.end[0] && v.start[1] === v.end[1]">
+              <div v-if="v.start && v.start[0] === v.end[0] && v.start[1] === v.end[1]">
                 <v-text-field v-model="editVehicles[i].start" :persistent-hint="true" :hint="'Start & End'"></v-text-field>
+              </div>
+              <div v-else-if="!v.start">
+                <v-text-field class="locationInput"
+                              v-model="model.placeName"
+                              :persistent-hint="true"  :hint="'Location'"
+                              @click="setFocus(true)"
+                              @keyup="locationInputChanged($event)">
+                </v-text-field>
+                <box background="white" v-if="placeSuggestions.length !== 0">
+                  <v-list-tile class="place-suggestion" :class="{'raw-coord': placeSuggested.rawCoordinate}" @click="selectSuggestion(placeSuggested)" :key="placeSuggested.id" v-for='placeSuggested in placeSuggestions'
+                               :title="placeSuggested.placeName.trim()">
+                    <v-list-tile-action class="hidden-sm-and-down">
+                      <v-icon v-if="placeSuggested.properties.layer === 'locality' || placeSuggested.properties.layer === 'city' || placeSuggested.properties.layer === 'county'">location_city</v-icon>
+                      <img alt="Icon or image for suggested place" width="25px" v-else-if="showAreaIcon(placeSuggested)" :src="getImgSrc('countryIconImgSrc')" height="auto" />
+                      <v-icon v-else>place</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title :title="placeSuggested.placeName.trim()">
+                        <v-btn v-html="highlightedName(placeSuggested.placeName)" style="min-width: fit-content" flat small @click.stop="selectSuggestion(placeSuggested)" class="no-padding no-margin no-capitalize">
+                        </v-btn>
+                      </v-list-tile-title>
+                      <v-list-tile-sub-title>
+                        {{ getLayerTranslation(placeSuggested.properties.layer) }}
+                        <span v-if="placeSuggested.properties.locality"> - {{ placeSuggested.properties.locality }} </span>
+                        <span v-if="placeSuggested.properties.country"> - {{ placeSuggested.properties.country }} </span>
+                        <span class="approximate-distance" :title="$t('placeInput.approximateDistance')">
+                          ~{{distance(placeSuggested)}}
+                          {{$t('global.units.' + $store.getters.mapSettings.unit)}}
+                        </span>
+                      </v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </box>
               </div>
               <div v-else>
                 <v-text-field v-model="editVehicles[i].start" :persistent-hint="true" :hint="'Start'"></v-text-field>
