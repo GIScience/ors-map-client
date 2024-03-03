a78150d1845c641cc942ff01b43bb23120f73001
Emma Hegarty <emma.hegarty@heigit.org>
2024-03-03 18:30:07 +0100

    fix: job icon

--- src/fragments/forms/map-form/components/optimization/components/job-list/JobList.vue
+++ src/fragments/forms/map-form/components/optimization/components/job-list/JobList.vue
@@ -2,7 +2,7 @@
   <div class="job-inputs">
       <v-expansion-panel  class="no-shadow" :value="jobExtended" :expand="true">
         <v-expansion-panel-content style="background: transparent;" v-for="(j, i) in jobs" :key="i">
-          <div slot="header" style="padding-bottom: 0;"><v-icon style="padding: 0 5px 0 0">local_shipping</v-icon><b>Job {{j.id}} - {{ j.location[0].toPrecision(8) }}, {{ j.location[1].toPrecision(8)}}</b></div>
+          <div slot="header" style="padding-bottom: 0;"><v-icon style="padding: 0 5px 0 0">work</v-icon><b>Job {{j.id}} - {{ j.location[0].toPrecision(8) }}, {{ j.location[1].toPrecision(8)}}</b></div>
           <v-card-text>
             <template v-for="prop in ['delivery','pickup','skills','time_window','service']">
               <v-chip v-if="j[prop] && prop === 'service'" style="flex: auto">{{ $t(`optimization.${prop}`) }}: {{ j[prop] }}</v-chip>
