7434bb4f3121ca5e5ff10daeffaaf8c9a097b426
Amandus Butzer <amandus.butzer@heigit.org>
2024-04-08 17:04:51 +0200

    refactor(footer): color links & use v-layout

--- src/fragments/footer/Footer.vue
+++ src/fragments/footer/Footer.vue
@@ -2,26 +2,16 @@
   <v-footer dark
   :style="{'margin-top': $vuetify.breakpoint.smAndDown ? '-' + $store.getters.footerMiniHeight + 'px': '-' + $store.getters.footerFullHeight + 'px'}"
   :height="$vuetify.breakpoint.smAndDown? $store.getters.footerMiniHeight : $store.getters.footerFullHeight" :class="{'small': $vuetify.breakpoint.smAndDown}">
-      <p>
-        <strong>
-          <a target="_blank" href="https://github.com/GIScience/ors-map-client">v{{ appVersion }}</a>
-        </strong>
-        <span>&nbsp;© {{currentYear}} <strong>{{footerMainSiteName}}</strong></span>
-        <span > | {{ $t('global.developedByIntro') }}
-          <strong>
-            <a target="_blank" :title="$t('global.developedBy')" :href="footerLink">{{ $t('global.developedBy') }}</a>
-          </strong>
-        </span><br>
-        <span>
-          <strong>
-            <a class="link" :title="$t('about.privacyPolicy')" :href="constants.links.privacyPolicy" target="_blank">{{$t('about.privacyPolicy')}}</a>
-          </strong>
-          &nbsp; &nbsp;
-          <strong>
-            <a class="link" :title="$t('about.termsOfService')" :href="constants.links.termsOfService" target="_blank">{{$t('about.termsOfService')}}</a>
-          </strong>
-        </span>
-      </p>
+      <v-layout justify-center column wrap >
+        <span><a :style="linkColor" target="_blank" href="https://github.com/GIScience/ors-map-client">v{{ appVersion }}</a>&nbsp;|&nbsp;© {{currentYear}} <strong>{{footerMainSiteName}}</strong></span>
+        <div>{{ $t('global.developedByIntro') }}
+            <a :style="linkColor" target="_blank" :title="$t('global.developedBy')" :href="footerLink">{{ $t('global.developedBy') }}</a>
+        </div><br>
+        <a :style="linkColor" class="link" :title="$t('about.privacyPolicy')" :href="constants.links.privacyPolicy"
+           target="_blank">{{ $t('about.privacyPolicy') }}</a>
+        <v-spacer shrink></v-spacer>
+        <a :style="linkColor" class="link" :title="$t('about.termsOfService')" :href="constants.links.termsOfService" target="_blank">{{$t('about.termsOfService')}}</a>
+      </v-layout>
     </v-footer>
 </template>
 
