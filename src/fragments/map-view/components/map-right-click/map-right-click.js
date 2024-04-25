66a453fb01f9e939773c560f2f825209750f1bc7
Emma Hegarty <emma.hegarty@heigit.org>
2024-04-25 15:56:27 +0200

    feat: add jobs/vehicles via right click on map

--- src/fragments/map-view/components/map-right-click/map-right-click.js
+++ src/fragments/map-view/components/map-right-click/map-right-click.js
@@ -48,7 +48,13 @@ export default {
       return this.$store.getters.mode === constants.modes.isochrones || (this.$store.getters.mode === constants.modes.place && !this.$store.getters.isSidebarVisible)
     },
     canRoute () {
-      return this.$store.getters.mode !== constants.modes.isochrones
+      return this.$store.getters.mode === constants.modes.directions || this.$store.getters.mode === constants.modes.place
+    },
+    canAddJob () {
+      return this.$store.getters.mode === constants.modes.optimization || (this.$store.getters.mode === constants.modes.place && !this.$store.getters.isSidebarVisible)
+    },
+    canAddVehicle () {
+      return this.$store.getters.mode === constants.modes.optimization || (this.$store.getters.mode === constants.modes.place && !this.$store.getters.isSidebarVisible)
     },
     canShowInspector () {
       return true
