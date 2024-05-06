0b2484c8ae08f4d34055421cdaaa7e0a6ef620fe
Emma Hegarty <emma.hegarty@heigit.org>
2024-05-06 15:52:48 +0200

    fix: vehicle time window

--- src/fragments/forms/map-form/components/optimization/components/edit-dialog/i18n/edit-dialog.i18n.hu-hu.js
+++ src/fragments/forms/map-form/components/optimization/components/edit-dialog/i18n/edit-dialog.i18n.hu-hu.js
@@ -1,5 +1,9 @@
 export default {
   editDialog: {
     keepEdits: 'Keep edits',
+    service: 'Service time (in seconds)',
+    timeWindow: ' of time window (in seconds passed since 00:00 or timestamp)',
+    start: 'Start',
+    end: 'End'
   }
 }
--- src/fragments/forms/map-form/components/optimization/components/vehicle-list/VehicleList.vue
+++ src/fragments/forms/map-form/components/optimization/components/vehicle-list/VehicleList.vue
@@ -4,7 +4,7 @@
       <v-card-title style="padding-bottom: 0;"><v-icon :color="vehicleColors(v.id)" style="padding: 0 5px 0 0">{{vehicleIcon(v.profile)}}</v-icon><b>Vehicle {{v.id}} ({{v.profile}})</b></v-card-title>
       <v-card-text>
           <template v-for="prop in ['capacity','skills','time_window']">
-            <v-chip v-if="v[prop] && prop === 'time_window' && v[prop].length" style="flex: auto">{{ $t(`optimization.${prop}`) }}: {{ v[prop] }}</v-chip>
+            <v-chip v-if="v[prop] && prop === 'time_window' && v[prop][0] > 0 " style="flex: auto">{{ $t(`optimization.${prop}`) }}: {{ v[prop][0] }} - {{ v[prop][1] }}</v-chip>
             <v-chip v-else-if="v[prop] && prop === 'capacity'" style="flex: auto">{{ $t(`optimization.${prop}`) }}: {{ v[prop][0] }}</v-chip>
             <v-chip v-else-if="v[prop] && prop === 'skills' && v[prop].length" style="flex: auto">
               {{ $t(`optimization.${prop}`) }}: {{ skillIds(v) }}
