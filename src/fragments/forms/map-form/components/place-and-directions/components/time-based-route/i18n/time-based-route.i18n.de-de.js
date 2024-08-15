42da72b88b421593f3a4a4252457b7119b3a7b62
Amandus Butzer <amandus.butzer@heigit.org>
2024-08-15 12:51:52 +0200

    refactor: remove unused translations

--- src/fragments/forms/map-form/components/place-and-directions/components/time-based-route/i18n/time-based-route.i18n.de-de.js
+++ src/fragments/forms/map-form/components/place-and-directions/components/time-based-route/i18n/time-based-route.i18n.de-de.js
@@ -1,12 +1,6 @@
 
 export default {
   timeBasedRoute: {
-    time: 'Zeitpunkt',
-    departure: 'Abreise',
-    hour: 'Uhr',
-    now: 'Aktuell',
-    inFewHours: 'In 2 Stunden',
-    hotDays: 'Hitzetag',
     morning: 'Morgens',
     noon: 'Mittags',
     afternoon: 'Nachmittags',
