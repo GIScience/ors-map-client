c4b48b4bbd913f4efcaa3bdb446d44cf8f68eff5
Amandus Butzer <amandus.butzer@heigit.org>
2025-03-24 15:47:11 +0100

    test: fix cypress config for vite

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require("cypress");
-const config = require('./config');
-const webpackPreprocessor = require('@cypress/webpack-preprocessor')
+const { defineConfig } = require('cypress');
+const vitePreprocessor = require('cypress-vite')
 
 module.exports = defineConfig({
   projectId: '2npvgh',
@@ -12,23 +11,13 @@ module.exports = defineConfig({
     ],
     baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      const options = {
-        // send in the options from your webpack.config.js, so it works the same
-        // as your app's code
-        webpackOptions: require('./build/webpack.base.conf'),
-        watchOptions: {},
-      }
-
-      on('file:preprocessor', webpackPreprocessor(options))
+      on('file:preprocessor', vitePreprocessor())
     },
   },
   component: {
     devServer: {
       framework: 'vue',
-      bundler: 'webpack',
-      // optionally pass in webpack config
-      webpackConfig: require('./build/webpack.cypress.conf'),
+      bundler: 'vite',
     },
     specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
     excludeSpecPattern: "src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}"
