cb285fc75bae175cac731984d8728aba22afee5e
Selina Breitenbach <selina.breitenbach@heigit.org>
2025-03-25 12:21:08 +0100

    build: use index.html for dev and production
    - remove unnecessary html plugin
    - do not ignore index.html in git
    - move configurations in preparation for production build

--- vite.config.js
+++ vite.config.js
@@ -2,35 +2,38 @@ import {defineConfig, loadEnv, mergeConfig} from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import { fileURLToPath, URL } from 'node:url'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
-import path from "path"
-import { createHtmlPlugin } from "vite-plugin-html"
+import path from 'path'
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   let viteConfig = {
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url)),
-          'vue$': 'vue/dist/vue.esm.js',
-          'fixtures': fileURLToPath(new URL('cypress/fixtures', import.meta.url))
-      }
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        'vue$': 'vue/dist/vue.esm.js',
+        'fixtures': fileURLToPath(new URL('cypress/fixtures', import.meta.url))
+    }
+  },
+    esbuild: {
+      minify: true
     },
     plugins: [
       vue(),
       viteCommonjs(),
-    ]
+    ],
+    static: {
+      directory: path.resolve(__dirname, './src/assets'),
+      publicPath: '/static',
+    }
   }
   if (command === 'serve') {
     //dev specific config
     let devConfig = {
-      define: {
-        __APP_ENV__: JSON.stringify(env.APP_ENV),
-        __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-      },
-      esbuild: {
-        minify: true
-      },
       server: {
         host: 'localhost',
         port: 8080,
@@ -43,19 +46,9 @@ export default defineConfig(({ command, mode }) => {
           overlay: { warnings: false, errors: true },
         }
       },
-      plugins: [
-        createHtmlPlugin({
-          filename: 'dev.html',
-          template: 'dev.html',
-        })
-      ],
       watch: {
         usePolling: true,
         interval: 1000,
-      },
-      static: {
-        directory: path.resolve(__dirname, './src/assets'),
-        publicPath: '/static',
       }
     }
     return mergeConfig(viteConfig, devConfig)
