27a7b994debdd3570e871d22c0a5fc42dc14df22
Selina Breitenbach <selina.breitenbach@heigit.org>
2025-04-10 17:23:54 +0200

    fix: change v-content to v-main in vuetify application

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-progress-linear :indeterminate="true" class="progress-linear" :class="{'progress-linear-disabled': !showLoading}"></v-progress-linear>
     <app-toaster></app-toaster>
     <app-header :class="{'a11y' : $store.getters.mapSettings.accessibleModeActive}"></app-header>
-    <v-content class="app-content" :class="{'a11y' : $store.getters.mapSettings.accessibleModeActive}">
+    <v-main class="app-content" :class="{'a11y' : $store.getters.mapSettings.accessibleModeActive}">
       <app-confirm></app-confirm>
       <app-info></app-info>
       <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" mode="out-in">
         <router-view v-title="title"></router-view>
       </transition>
-    </v-content>
+    </v-main>
     <app-sidebar :class="{'a11y' : $store.getters.mapSettings.accessibleModeActive}"></app-sidebar>
   </v-app>
 </template>
