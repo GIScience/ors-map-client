154b558244ed1990d88deb869365aff139edb48d
Selina Breitenbach <selina.breitenbach@heigit.org>
2025-06-18 10:52:39 +0200

    fix: whitespace missing

--- src/fragments/html-marker/HtmlMarker.vue
+++ src/fragments/html-marker/HtmlMarker.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div :style="{background: color}" class="custom-html-icon-div">
-      <img alt="Map marker icon"v-if="markerImg" class="custom-html-icon-img" :src="markerImg">
+      <img alt="Map marker icon" v-if="markerImg" class="custom-html-icon-img" :src="markerImg">
       <div v-else-if="markerNumber" class="custom-html-icon-txt" :class="{'double-digit-text': doubleDigitText, 'triple-digit-text': tripleDigitText, 'four-digit-text': fourDigitText}">{{markerNumber}} </div>
     </div>
   </div>
