6cf8548c21454d6269fe1946efd7bcef2ee0291f
Amandus Butzer <amandus.butzer@heigit.org>
2025-07-17 13:23:24 +0200

    wip: adjust plugin example to only have appLoaded hook and log

--- src/plugins/plugin-example/plugin-example.js
+++ src/plugins/plugin-example/plugin-example.js
@@ -20,6 +20,10 @@ class PluginExample {
     this.vueInstance = vueInstance
   }
 
+  appLoaded () {
+    console.log('App Loaded Plugin Example')
+  }
+
   /**
    * Is called when the app is loaded and the map view is ready
    *
@@ -27,29 +31,29 @@ class PluginExample {
    * hooks.js on the mapReady hook or automatically via catchAll
    * @param {Object} hookData
    */
-  mapReady (hookData) {
-    console.log('PluginExample: mapReady callback', hookData)
-    // hookData has the following structure {context: Object, map: Object}
-    // The leaflet `map` object is passed, and
-    // you can potentially change it by adding or removing controls
-  }
-
-  /**
-   * Method to get the current mapViewData and potentially
-   * change it. As it is an object, when you change it you are
-   * changing the original object.
-   * The MapView is watching to changes to this object and
-   * will re-render the displayed content on the map view
-   * when it is changed.
-   * IMPORTANT: this method is expected to be called on
-   * hooks.js on the mapViewDataChanged hook.
-   * @param {*} mapViewData
-   */
-  mapViewDataChanged (mapViewData) {
-    console.log('PluginExample: mapViewDataChanged callback', mapViewData)
-    // change the mapViewData object
-    // so that it will update the map view
-  }
+  // mapReady (hookData) {
+  //   console.log('PluginExample: mapReady callback', hookData)
+  //   // hookData has the following structure {context: Object, map: Object}
+  //   // The leaflet `map` object is passed, and
+  //   // you can potentially change it by adding or removing controls
+  // }
+  //
+  // /**
+  //  * Method to get the current mapViewData and potentially
+  //  * change it. As it is an object, when you change it you are
+  //  * changing the original object.
+  //  * The MapView is watching to changes to this object and
+  //  * will re-render the displayed content on the map view
+  //  * when it is changed.
+  //  * IMPORTANT: this method is expected to be called on
+  //  * hooks.js on the mapViewDataChanged hook.
+  //  * @param {*} mapViewData
+  //  */
+  // mapViewDataChanged (mapViewData) {
+  //   console.log('PluginExample: mapViewDataChanged callback', mapViewData)
+  //   // change the mapViewData object
+  //   // so that it will update the map view
+  // }
 }
 // export the AppHooks json builder class
 export default PluginExample
