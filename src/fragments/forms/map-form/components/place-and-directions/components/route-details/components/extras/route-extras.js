8501015f0e55501f900eb0f824db498e322a0fb1
jstier <stier.jochen@web.de>
2026-04-30 14:43:38 +0200

    fix: remove duplication

--- src/fragments/forms/map-form/components/place-and-directions/components/route-details/components/extras/route-extras.js
+++ src/fragments/forms/map-form/components/place-and-directions/components/route-details/components/extras/route-extras.js
@@ -1,6 +1,7 @@
 import orsDictionary from '@/resources/ors-dictionary'
 import {EventBus} from '@/common/event-bus'
 import Utils from '@/support/utils'
+import {buildExtraHighlightPolylineData} from '@/support/heal-utils'
 
 export default {
   data () {
@@ -71,21 +72,6 @@ export default {
       return show
     },
     /**
-     * Determines if the extras block must
-     * be shown, checking if exists
-     * extra data
-     * @returns {Boolean}
-     */
-    showExtras () {
-      let extras = 0
-      for (const extraKey in this.routeExtras) {
-        if (this.showExtra(extraKey)) {
-          extras++
-        }
-      }
-      return extras > 0
-    },
-    /**
      * Get the color from the ors dictionary
      * based on the extra key and index
      * @param {*} extraKey
@@ -153,7 +139,7 @@ export default {
       const color = this.colorValue(extraKey, index)
       const highlightData = { extraKey, sectionTitle, sections: [{ intervals: [], color }] }
 
-      const polylineData = this.buildExtraHighlightPolylineData(extraKey, index, value)
+      const polylineData = buildExtraHighlightPolylineData(extraKey, index, value, this.$t)
       highlightData.sections.push(polylineData)
       EventBus.$emit('highlightPolylineSections', highlightData)
     },
@@ -173,44 +159,12 @@ export default {
 
       let index = 0
       for (const summary of this.routeExtras[extraKey].summary) {
-        const polylineData = this.buildExtraHighlightPolylineData(extraKey, index, summary.value)
+        const polylineData = buildExtraHighlightPolylineData(extraKey, index, summary.value, this.$t)
         highlightData.sections.push(polylineData)
         index++
       }
       // This is disabled now since we do styling inside map-view.js
       // EventBus.$emit('highlightPolylineSections', highlightData)
     },
-    /**
-     * Build the extra info highlighting data
-     * @param {String} extraKey
-     * @param {Integer} index
-     * @param {Integer} value
-     * @returns {Object} {intervals: Array, color: string, label: String}
-     */
-    buildExtraHighlightPolylineData (extraKey, index, value) {
-      const color = this.colorValue(extraKey, index, value)
-      const label = this.getExtraValueLabel(extraKey, value).toLowerCase()
-      // Values contains an array with the following data:
-      // a) position `zero` - the starting index on the route polyline array of
-      // where the given extra info starts
-      // b) position `1` - the final index on the route polyline array where the
-      // given extra info ends.
-      // c) position 2 - the value that represents the extra info to be
-      // shown on over the route. For example, steepness
-      const values = this.routeExtras[extraKey].values
-
-
-      // As some extra info may be present in several non-continuous
-      // segments we must get the intervals where the value matches
-      // so that we show only the extra info wth the value selected by the user
-      const intervals = this.lodash.filter(values, (v) => {
-        return v[2] === value
-      })
-      return {
-        intervals,
-        color,
-        label
-      }
-    }
   }
 }
