70f70130b35362890216b17229cdee1b40a5596b
jstier <stier.jochen@web.de>
2026-04-30 15:38:55 +0200

    feat: add width percentage under bars

--- src/fragments/forms/map-form/components/place-and-directions/components/route-details/components/extras/RouteExtras.vue
+++ src/fragments/forms/map-form/components/place-and-directions/components/route-details/components/extras/RouteExtras.vue
@@ -1,24 +1,33 @@
 <template>
-<div>
-      <template v-for="(extra, extraKey) in routeExtras">
-        <v-layout row wrap :key="extraKey" v-if="showExtra(extraKey)">
-            <v-list style ="width:95%;">
-              <div style="padding:0 0 0 10px">
-                <h5 >{{$t('global.' + extraKey )}}</h5>
-                <div class="extra-bar">
-                  <template v-for="(summary, index) in routeExtras[extraKey].summary">
-                    <v-tooltip :key="index + extraKey" :disabled="$lowResolution" top>
-                      <div  slot="activator" class="segment" :style="segmentStyle(extraKey, summary, index)"></div>
-                      {{getExtraValueLabel(extraKey, summary.value)}}
-                    </v-tooltip>
-                  </template>
+  <div>
+    <template v-for="(extra, extraKey) in routeExtras">
+      <v-layout row wrap :key="extraKey" v-if="showExtra(extraKey)">
+        <v-list style="width:95%;">
+          <div style="padding:0 0 0 10px">
+            <div class="extra-bar">
+              <template v-for="(summary, index) in routeExtras[extraKey].summary">
+                <v-tooltip :key="index + extraKey" :disabled="$lowResolution" top>
+                  <div slot="activator" class="segment" :style="segmentStyle(extraKey, summary, index)"></div>
+                  {{ getExtraValueLabel(extraKey, summary.value) }}
+                </v-tooltip>
+              </template>
+            </div>
+            <div class="extra-bar-labels">
+              <template v-for="(summary, index) in routeExtras[extraKey].summary">
+                <div
+                  :key="'label-' + index + extraKey"
+                  class="segment-label"
+                  :style="{ '--seg-width': summary.amount + '%' }"
+                >
+                  {{ getExtraValueLabel(extraKey, summary.value) }} {{ summary.amount.toFixed(1) }} %
                 </div>
-              </div>
-            </v-list>
-
-        </v-layout>
-      </template>
-</div>
+              </template>
+            </div>
+          </div>
+        </v-list>
+      </v-layout>
+    </template>
+  </div>
 </template>
 
 <script src="./route-extras.js"></script>
