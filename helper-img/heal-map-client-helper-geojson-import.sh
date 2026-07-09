f6594ddfa49d5fde10fb2d5974493a735a974bd9
jstier <stier.jochen@web.de>
2026-07-09 18:03:28 +0200

    feat: adjust heal map client helper to new structure

--- helper-img/heal-map-client-helper-geojson-import.sh
+++ helper-img/heal-map-client-helper-geojson-import.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 # ORS heal-map-client-helper-geojson-import
 ########################################################################################################################
-rclone config create heal s3 --non-interactive --quiet \
+rclone config create heal s3 --non-interactive --quiet  \
   provider=Minio \
   access_key_id="$HEAL_KEY_ID" \
   secret_access_key="$HEAL_ACCESS_KEY" \
   endpoint="$HEAL_URL" \
   acl=private
 
-echo "Downloading index.json..."
-rclone copyto heal:/"$HEAL_BUCKET"/"$HEAL_PREFIX"/output/index.json ./aois/index.json --quiet
+echo "Downloading countries.json..."
+rclone copyto heal:/"$HEAL_BUCKET"/"$HEAL_PREFIX"/output/countries.json ./aois/countries.json --quiet
 
 ########################################################################################################################
 echo "Downloading corresponding GeoJSON files..."
 mkdir -p aois
-
-while IFS= read -r state; do
-  while IFS= read -r city; do
-    echo "  Downloading germany/aois/${state}/${city}.geojson..."
-    rclone copyto \
-      heal:/"$HEAL_BUCKET"/"$HEAL_PREFIX"/output/aois/germany/"${state}"/"${city}".geojson \
-      ./aois/"${state}"/"${city}".geojson --quiet
-  done < <(jq -r --arg s "$state" '.[$s][]' ./aois/index.json)
-done < <(jq -r 'keys[]' ./aois/index.json)
-
+while IFS= read -r country; do
+  while IFS= read -r state; do
+    while IFS= read -r city; do
+      echo "  Downloading /aois/${country}/${state}/${city}.geojson..."
+      rclone copyto \
+        heal:/"$HEAL_BUCKET"/"$HEAL_PREFIX"/output/aois/"${country}"/"${state}"/"${city}".geojson \
+        ./aois/"${country}"/"${state}"/"${city}".geojson --quiet
+    done < <(jq -r --arg c "$country" --arg s "$state" '.[$c][$s][]' ./aois/countries.json)
+  done < <(jq -r --arg c "$country" '.[$c] | keys[]' ./aois/countries.json)
+done < <(jq -r 'keys[]' ./aois/countries.json)
 find ./aois
 
 echo "HEAL GeoJSON import completed successfully."
